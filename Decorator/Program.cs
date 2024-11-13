1c2e5d02a67b0161638124931fbcd910777dd05a
KSavycheva <savychevakateryna@knu.ua>
2024-11-13 02:44:56 +0200

    Add files via upload

---
+++ Decorator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            ChristmasTree ourTree = new ChristmasTree();
+            DecorateWithToys toys = new DecorateWithToys();
+            DecorateWithTinsel tinsel = new DecorateWithTinsel();
+            DecorateWithTinsel tinsel2 = new DecorateWithTinsel();
+            DecorateWithGarland garland = new DecorateWithGarland();
+
+            //прикрашаємо нашу ялинку іграшками, двома дощиками та гірляндою
+            toys.SetComponent(ourTree);
+            tinsel.SetComponent(toys);
+            tinsel2.SetComponent(tinsel);
+            garland.SetComponent(tinsel2);
+
+            garland.Operation();
+            garland.TurnOn(); // один раз гірлянду ввімкнув - свято більше не закінчується!
+
+            Console.Read();
+        }
+    }
+  
+    abstract class Fir
+    {
+        public abstract void Operation();
+    }
+
+    class ChristmasTree : Fir
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Ялинка стоїть посеред кімнати");
+        }
+    }
+  
+    abstract class Decorator : Fir
+    {
+        protected Fir component;
+
+        public void SetComponent(Fir component)
+        {
+            this.component = component;
+        }
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    class DecorateWithToys : Decorator
+    {
+        private string addedState;
+
+        public override void Operation()
+        {
+            base.Operation();
+            addedState = "Іграшки";
+            Console.WriteLine("Прикрашена іграшками");
+        }
+    }
+
+    class DecorateWithTinsel : Decorator
+    {
+        private string addedState;
+
+        public override void Operation()
+        {
+            base.Operation();
+            addedState = "Дощик";
+            Console.WriteLine("Прикрашена дощиком");
+        }
+    }
+
+    class DecorateWithGarland : Decorator
+    {
+        private string garland;
+        public override void Operation()
+        {
+            base.Operation();
+            garland = "Гірлянда";
+            Console.WriteLine("Прикрашена гірляндою");
+        }
+        public void TurnOn()
+        {
+            Console.WriteLine("Вона світиться різними кольорами...");
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,91 @@
+﻿using PrototypeFigure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IFigure figure = new Rectangle(10, 20);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+
+            IFigure triangle = new Triangle(5, 8);
+            IFigure clonedTriangle = triangle.Clone();
+            triangle.GetInfo();
+
+            Console.Read();
+        }
+    }
+
+    interface IFigure
+ {
+ IFigure Clone();
+    void GetInfo();
+}
+
+    class Triangle : IFigure
+    {
+        int baseLength;
+        int height;
+
+        public Triangle(int b, int h)
+        {
+            baseLength = b;
+            height = h;
+        }
+
+        public IFigure Clone()
+        {
+            return new Triangle(this.baseLength, this.height);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Трикутник з основою {0} та висотою {1}", baseLength, height);
+        }
+
+    }
+
+    class Rectangle : IFigure
+{
+    int width;
+    int height;
+    public Rectangle(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+    public IFigure Clone()
+    {
+        return new Rectangle(this.width, this.height);
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine("Прямокутник довжиною {0} и шириною {1} ", height, width);
+    }
+}
+class Circle : IFigure
+{
+    int radius;
+    public Circle(int r)
+    {
+        radius = r;
+    }
+    public IFigure Clone()
+    {
+        return new Circle(this.radius);
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine("Круг радіусом {0}", radius);
+    }
+}
+}
